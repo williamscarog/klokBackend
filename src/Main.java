fb7fd462a7c3c5ec331563d1b69764fb199253ca
Williams Caro G. <williamscaro194@gmail.com>
2024-09-26 21:05:44 -0400

    Ahora es funcional! Hemos resuelto el problema utilizando funciones para que el menu vuelva a aparecer.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        welcome();
+        options();
+    }
+
+    //metodo para bienvenida
+    static void welcome() {
         // Colores ANSI para la consola (si tu terminal los soporta)
         String ANSI_RESET = "\u001B[0m";
         String ANSI_YELLOW = "\u001B[33m";
@@ -23,13 +29,22 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         System.out.println(ANSI_RED +"\nPresione 'Enter' para continuar..."+ ANSI_RESET);
         teclado.nextLine();  // Espera a que el usuario presione Enter
-
         System.out.println(ANSI_BLUE + "═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════" + ANSI_RESET);
+
+    }
+
+    static void options(){
+        Scanner teclado = new Scanner(System.in);
+        int saldoDisponible = 1000;
+        int retiro = 0;
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_YELLOW = "\u001B[33m";
+        String ANSI_CYAN = "\u001B[36m";
+
+        //AQUI SE REPITE
         System.out.println("Cliente: Williams Caro Galvan");
         System.out.println("Tipo de Cuenta: Ahorros");
-
-
-
+        System.out.println("Saldo disponible $DOP:"+ saldoDisponible);
         // Otras informaciones útiles
         System.out.println(ANSI_YELLOW + "\nPor favor, elija una de las siguientes opciones para continuar:" + ANSI_RESET);
         System.out.println(ANSI_CYAN + "1. Consultar Saldo");
@@ -37,14 +52,11 @@ public class Main {
         System.out.println("3.Depositar");
         System.out.println("9. Salir" + ANSI_RESET);
 
-//        Scanner teclado = new Scanner(System.in);  // Create a Scanner object
-        int saldoDisponible = 1000;
-        int retiro = 0;
         int option = teclado.nextInt();
-
         switch (option){
             case 1:
                 System.out.println("Su saldo es de: $DOP: "+ saldoDisponible);
+                options();
                 break;
 
 
@@ -54,11 +66,11 @@ public class Main {
                 if (retiro > saldoDisponible || saldoDisponible == 0){
                     System.out.println("Saldo Insuficiente");
                 }else {
-                    int balanceActualizado = retiro - saldoDisponible;
+                    int balanceActualizado = saldoDisponible - retiro ;
                     saldoDisponible = balanceActualizado;
                     System.out.println("Retiro de $DOP: "+retiro+". Aplicado. Su nuevo balance ahora es de $DOP: "+saldoDisponible);
                 }
-
+                options();
                 break;
 
             case 3:
@@ -66,6 +78,7 @@ public class Main {
                 int deposito = teclado.nextInt();
                 saldoDisponible = deposito + saldoDisponible;
                 System.out.println("Su nuevo saldo es de: DOP$: "+ saldoDisponible);
+                options();
                 break;
 
             case 9:
